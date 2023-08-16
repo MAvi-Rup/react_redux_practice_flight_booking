491f09b4c63d918496bec00bda35716890b7cd57
MAvi-Rup <avirupmondal19@gmail.comm>
2023-08-17 02:13:06 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './styles.css';
 import Header from "./components/Header"
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import InputSection from './components/Booking/InputSection';
+import BookingList from './components/Booking/BookingList';
 
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
+      <InputSection />
+      <BookingList />
      
     </Provider>
   );
